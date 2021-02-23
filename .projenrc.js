2c65e5cbbfdbba7fd2e60fd517956410f37b7e50
Pahud <pahudnet@gmail.com>
2021-02-24 00:04:06 +0800

    rename

--- .projenrc.js
+++ .projenrc.js
@@ -3,8 +3,8 @@ const { TypeScriptProject } = require('projen');
 const project = new TypeScriptProject({
   defaultReleaseBranch: 'main',
   jsiiFqn: 'projen.TypeScriptProject',
-  name: '@pahud/projen-extensions',
-  description: 'Projen extensions common used by Pahud Hsieh',
+  name: 'projen-automate-it',
+  description: 'Projen wrapper for common operations',
   releaseBranches: ['main'],
   deps: ['projen'],
   dependabot: false,
